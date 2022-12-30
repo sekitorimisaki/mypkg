6478edf5e895d6ea458797a556b82e3daa935af6
sekitorimisaki <sekitorimisaki@DESKTOP-THJNPKR.localdomain>
2022-12-30 15:41:41 +0900

    コードの修正

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -5,20 +5,19 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-class Talker():         
-    def __init__(self):  
+class Talker():
+    def __init__(self, node):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
+        node.create_timer(0.5, cb)
  
+    def cb(self):      #インデントをあげてselfを引数に
+        msg = Int16()
+        msg.data = self.n     
+        self.pub.publish(msg) 
+        self.n += 1      
+     
 rclpy.init()
 node = Node("talker")
-talker = Talker()
-
-def cb():             
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
-
-node.create_timer(0.5, cb)
+talker = Talker(node)
 rclpy.spin(node)
