816c0bc932a917d03e0522a825521bd2b59e6e97
sekitorimisaki <sekitorimisaki@DESKTOP-THJNPKR.localdomain>
2022-12-30 15:35:53 +0900

    コードの修正

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -9,7 +9,7 @@ class Talker():
     def __init__(self):  
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
-        node.create_timer(0.5, cb) 
+ 
 rclpy.init()
 node = Node("talker")
 talker = Talker()
